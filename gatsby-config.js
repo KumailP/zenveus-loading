53d590419584374115191eb9c533502e7e3fafc0
Kumail Pirzada <kumailpirzada@gmail.com>
2020-02-11 18:09:25 +0500

    Updatessssssss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Zenveus - coming soon`,
+    description: `Zenveus - coming soon`,
+    author: `@kumailp`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
